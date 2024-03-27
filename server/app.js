30c3a0dac0790b4d7d7a695cff7f2a48cb42a29b
Sachin Kadian <sachinkadian2019@gmail.com>
2024-03-27 17:39:07 +0530

    dotenv added

--- server/app.js
+++ server/app.js
@@ -6,4 +6,9 @@ app.get('/',(req,res)=>{
     res.send("Hello Sachin, Myself ProMailer");
 })
 
+
+app.get('/check',(req,res)=>{
+    res.send("Hello Sachin, Check This Side...");
+})
+
 module.exports = app;
